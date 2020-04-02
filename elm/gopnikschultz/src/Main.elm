d5560c81d108fe6c07a632bebf899b1f45087507
Kate Lin <kslin@mit.edu>
2020-04-01 20:40:17 -0700

    change indents to 2 spaces

--- elm/gopnikschultz/src/Main.elm
+++ elm/gopnikschultz/src/Main.elm
@@ -8,77 +8,77 @@ import Svg.Attributes exposing (width, height, x, y, viewBox, xlinkHref)
 
 -- MAIN
 main =
-    Browser.sandbox {init = init, update = update, view = view}
+  Browser.sandbox {init = init, update = update, view = view}
 
 alarm = image [xlinkHref "data/alarm.svg", 
-                width "200", height "200", x "130", y "0"][]
+        width "200", height "200", x "130", y "0"][]
 silence = svg[][]
 
 --EDIT THIS LOGIC FOR DIFF CASES
 -- Currently implementing one-cause condition w/ Yellow Block
 playAlarm blicket=
-    if blicket == "none" then
-        silence
-    else if blicket == "boxAndCylinder" then
-        alarm
-    else if blicket == "yellowBox" then
-        alarm
-    else if blicket == "blueCylinder" then
-        silence
-    else
-        silence
+  if blicket == "none" then
+    silence
+  else if blicket == "boxAndCylinder" then
+    alarm
+  else if blicket == "yellowBox" then
+    alarm
+  else if blicket == "blueCylinder" then
+    silence
+  else
+    silence
 
 --Images of blickets being placed on sensor
 sensor = svg [ width "500"
-                    , height "500"
-                    , viewBox "0 0 500 500"
-                    ] [
-                        image [
-                                xlinkHref "data/sensor.svg", 
-                                width "300", height "300", x "75", y "120"][]
-                        , playAlarm "none"
-                    ]
+          , height "500"
+          , viewBox "0 0 500 500"
+          ] [
+            image [
+                xlinkHref "data/sensor.svg", 
+                width "300", height "300", x "75", y "120"][]
+            , playAlarm "none"
+          ]
 yellowBox = svg [ width "500"
-                    , height "500"
-                    , viewBox "0 0 500 500"
-                    ] [
-                        image [
-                                xlinkHref "data/sensor.svg", 
-                                width "300", height "300", x "75", y "120"][]
-                        , image [
-                                xlinkHref "data/yellowbox.svg", 
-                                width "100", height "100", x "180", y "130"][]
-                        , playAlarm "yellowBox"     
-                    ]
+          , height "500"
+          , viewBox "0 0 500 500"
+          ] [
+            image [
+                xlinkHref "data/sensor.svg", 
+                width "300", height "300", x "75", y "120"][]
+            , image [
+                xlinkHref "data/yellowbox.svg", 
+                width "100", height "100", x "180", y "130"][]
+            , playAlarm "yellowBox"     
+          ]
 
 blueCylinder =  svg [ width "500"
-                    , height "500"
-                    , viewBox "0 0 500 500"
-                    ] [
-                        image [
-                                xlinkHref "data/sensor.svg", 
-                                width "300", height "300", x "75", y "120"][],
-                        image [
-                                xlinkHref "data/bluecylinder.svg", 
-                                width "100", height "100", x "180", y "130"][]
-                        , playAlarm "blueCylinder"     
-                    ]
+          , height "500"
+          , viewBox "0 0 500 500"
+          ] [
+            image [
+                xlinkHref "data/sensor.svg", 
+                width "300", height "300", x "75", y "120"][],
+            image [
+                xlinkHref "data/bluecylinder.svg", 
+                width "100", height "100", x "180", y "130"][]
+            , playAlarm "blueCylinder"     
+          ]
 
 boxAndCylinder = svg [ width "500"
-                    , height "500"
-                    , viewBox "0 0 500 500"
-                    ] [
-                        image [
-                                xlinkHref "data/sensor.svg", 
-                                width "300", height "300", x "75", y "120"][],
-                        image [
-                                xlinkHref "data/bluecylinder.svg", 
-                                width "100", height "100", x "120", y "130"][],
-                        image [
-                                xlinkHref "data/yellowbox.svg", 
-                                width "100", height "100", x "250", y "130"][]
-                        , playAlarm "boxAndCylinder"     
-                    ]
+          , height "500"
+          , viewBox "0 0 500 500"
+          ] [
+            image [
+                xlinkHref "data/sensor.svg", 
+                width "300", height "300", x "75", y "120"][],
+            image [
+                xlinkHref "data/bluecylinder.svg", 
+                width "100", height "100", x "120", y "130"][],
+            image [
+                xlinkHref "data/yellowbox.svg", 
+                width "100", height "100", x "250", y "130"][]
+            , playAlarm "boxAndCylinder"     
+          ]
 
 -- MODEL
 type alias Model = Html Msg 
@@ -91,15 +91,15 @@ init = sensor
 view : Model -> Html Msg 
 
 view model =
-    div [][
-        div [] [
-                button [onClick Original] [ Html.text "Original"],
-                button [onClick Cylinder] [Html.text "Blue Cylinder"],
-                button [onClick Box] [Html.text "Yellow Box"],
-                button [onClick Both] [Html.text "Both"]
-                ],
-        div [][model]
-    ]
+  div [][
+    div [] [
+        button [onClick Original] [ Html.text "Original"],
+        button [onClick Cylinder] [Html.text "Blue Cylinder"],
+        button [onClick Box] [Html.text "Yellow Box"],
+        button [onClick Both] [Html.text "Both"]
+        ],
+    div [][model]
+  ]
 
 -- UPDATE
 
@@ -108,9 +108,9 @@ type Msg = Original | Cylinder | Box | Both
 update: Msg -> Model -> Model
 
 update msg model = 
-    case msg of
-        Original -> sensor
-        Cylinder -> blueCylinder
-        Box -> yellowBox
-        Both -> boxAndCylinder
+  case msg of
+    Original -> sensor
+    Cylinder -> blueCylinder
+    Box -> yellowBox
+    Both -> boxAndCylinder
 
