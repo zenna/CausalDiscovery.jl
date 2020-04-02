171fcdfc1427b6ebf30b09c5b9a6b3da1ab0caad
Elizabeth <elizabethweeks22@gmail.com>
2020-04-02 09:30:49 -0700

    altered test file

--- test/MCMC.jl
+++ test/MCMC.jl
@@ -1,7 +1,5 @@
-using CausalDiscovery.MCMC:
-using Test
-
-include("../src/MCMC.jl")
+import Pkg; Pkg.add("CausalDiscovery")
+using CausalDiscovery.MCMC
 
 #tree1
 # TaggedParseTree(
@@ -16,50 +14,51 @@ include("../src/MCMC.jl")
 #             ])
 #         ])
 #     ])
+@testset "MCMC" begin
+    tree1 = generateTree(100)
+    proposed = proposeTree(tree1)
 
-tree1 = generateTree(100)
-proposed = proposeTree(tree1)
+    #tree1 should equal proposed expect with float_var_5 replacing float_var_1
+    #following test should be added when expressions are able to be evaluated
+    #@test(isapprox(eval(getExpr(tree1)), eval(getExpr(proposed)); 0.01)
 
-#tree1 should equal proposed expect with float_var_5 replacing float_var_1
-#following test should be added when expressions are able to be evaluated
-#@test(isapprox(eval(getExpr(tree1)), eval(getExpr(proposed)); 0.01)
+    #Tests findNodeWithPosition function
+    expected_node = tree1.root_node.children[1].children[1]
+    found_node = findNodeWithPosition(tree1.root_node, expected_node.node_position)
+    @test(expected_node == found_node)
 
-#Tests findNodeWithPosition function
-expected_node = tree1.root_node.children[1].children[1]
-found_node = findNodeWithPosition(tree1.root_node, expected_node.node_position)
-@test(expected_node == found_node)
+    #Testing random seed version of generateTree
+    tree2 = generateTree(100)
+    @test(tree1.node_positions == tree2.node_positions)
 
-#Testing random seed version of generateTree
-tree2 = generateTree(100)
-@test(tree1.node_positions == tree2.node_positions)
+    expr1 = getExpr(tree1)
+    expr2 = getExpr(tree2)
+    @test(expr1==expr2)
 
-expr1 = getExpr(tree1)
-expr2 = getExpr(tree2)
-@test(expr1==expr2)
+    #test getPriorProb with exogenous line
+    priorProb = getPriorProb(tree1)
+    expectedProb = 0.00625
+    @test(isapprox(priorProb, expectedProb; .01))
 
-#test getPriorProb with exogenous line
-priorProb = getPriorProb(tree1)
-expectedProb = 0.00625
-@test(isapprox(priorProb, expectedProb; .01))
+    #test getPriorProb with proposed tree
+    priorProb = getPriorProb(proposed)
+    @test(isapprox(priorProb, expectedProb; .01))
 
-#test getPriorProb with proposed tree
-priorProb = getPriorProb(proposed)
-@test(isapprox(priorProb, expectedProb; .01))
+    #testing conditional
+    expectedConditional = log((1/6)*(1/5) + (1/6)*.00625 + (1/30)*(1/8) + (1/30)*(1/16))
+    conditional = getConditionalLogProb(tree1, proposed)
+    @test(isapprox(conditional, expectedConditional; .01))
 
-#testing conditional
-expectedConditional = log((1/6)*(1/5) + (1/6)*.00625 + (1/30)*(1/8) + (1/30)*(1/16))
-conditional = getConditionalLogProb(tree1, proposed)
-@test(isapprox(conditional, expectedConditional; .01))
+    tree3 = generateTree(4)
 
-tree3 = generateTree(4)
+    #Test getPriorProb with endogenous line
+    priorProb = getPriorProb(tree3)
+    expectedProb = 1/(2*2*3*4*5*5)
+    @test(isapprox(priorProb, expectedProb; 0.01))
 
-#Test getPriorProb with endogenous line
-priorProb = getPriorProb(tree3)
-expectedProb = 1/(2*2*3*4*5*5)
-@test(isapprox(priorProb, expectedProb; 0.01))
-
-proposed = proposeTree(tree3)
-#Tests for conditional prob with endo line
-expectedConditional = log((1/6)*(1/25)*((1/48)+(1/24)+(1/12)+(1/4)))
-conditional = getConditionalLogProb(tree3, proposed)
-@test(isapprox(conditional, expectedConditional; 0.01))
+    proposed = proposeTree(tree3)
+    #Tests for conditional prob with endo line
+    expectedConditional = log((1/6)*(1/25)*((1/48)+(1/24)+(1/12)+(1/4)))
+    conditional = getConditionalLogProb(tree3, proposed)
+    @test(isapprox(conditional, expectedConditional; 0.01))
+end
