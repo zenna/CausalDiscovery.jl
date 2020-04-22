24a92857b145013f11870c50b8bd8575f422e106
Zenna Tavares <zennatavares@gmail.com>
2020-04-22 00:10:49 -0400

    subscriptions

--- CISC/src/Main.elm
+++ CISC/src/Main.elm
@@ -6,64 +6,34 @@ import Color
 import Update exposing (..)
 
 
--- A fake image
-gamewidth = 8
-gameheight = 8
+-- Car Example
 
--- fakerenderpixel : Int -> Int -> Position -> Pixel
--- fakerenderpixel maxwidth maxheight ((x, y) as pos) =
---   let
---     max = maxwidth + maxheight
---     val = x + y
---     i = toFloat val / 10.0
---   in
---   { rgba = Color.rgba 1.0 0.0 i i, pos = pos }
-
-
-
--- fakeimage : Image
--- fakeimage =
---   {
---     pixels = List.map (\pos -> fakerenderpixel gamewidth gameheight pos) (grid gamewidth gameheight),
---     width = gamewidth,
---     height = gameheight
---   }
-
-
--- -- Car Example
 car = Box (1, 1) 3 4 Color.yellow
 tank = Box (2, 2) 1 2 Color.red
-
-
 scene : Scene
 scene = [ObjectTag tank, ObjectTag car]
-image = Engine.render scene gamewidth gameheight
+gasPumpPos = (1, 1)
+gasPumpPressed computer = computer == gasPumpPos
+maxGas = 3
 
-main = pomdp
+move object x y =
+  case object of
+    Box (x_, y_) width height color -> Box (x + x_, y + y_) width height color
+    Circle -> Circle
+
+update computer {objects, latent} = 
+  let
+    tankLevel = latent
+    newTank = if gasPumpPressed computer then maxGas else tankLevel - 1
+    moveObjects = List.map (\o -> move o 1 1) objects
+  in
+  { 
+    objects = moveObjects,
+    latent = newTank
+  }
+  
+main = pomdp {objects = scene, latent = maxGas} update
 
 
-
--- tankValue : Int
--- tankValue = 0
-
-
--- car = Box (1, 1) 3 4 Color.red
-
--- update scene
-
--- -- Move the car in a random ddirection 
-
--- Render fake image
-htmlwidth = 400
-htmlheight = 400
--- main = view image htmlwidth htmlheight
-
-viewer computer (x,y) =
-  view image htmlwidth htmlheight
-
-update computer state =
-  state
-
-main =
-  game viewer update (0,0)
--- main = Render.main
+-- Haven't figured out messages
+-- I want to visuals to change as a function of the tank
--- CISC/src/Update.elm
+++ CISC/src/Update.elm
@@ -1,37 +1,111 @@
 module Update exposing (..)
 import Browser
 import Html exposing (Html, text, pre)
--- import Http
 import Render
 import Engine
+import Time
+import Browser.Events as E
+import Json.Decode as D
 
--- So the choice is either :
--- 1. rejig it to use playground
--- 2. implement dynamics myself
 
-
--- Why not just use playground?
--- Not sure how rendering works, when translating to julia ll have to copy what he did
-
--- pros of it
--- he has a ince interface for time, keyboard,
 htmlwidth = 400
 htmlheight = 400
 gamewidth = 8
 gameheight = 8
 
-observe computer state =
+-- SUBSCRIPTIONS
+
+type Msg
+  = KeyChanged Bool String
+  | Tick Time.Posix
+  -- | GotViewport Dom.Viewport
+  -- | Resized Int Int
+  -- | VisibilityChanged E.Visibility
+  | MouseMove Float Float
+  | MouseClick
+  | MouseButton Bool
+
+pomdpSubscriptions : Sub Msg
+pomdpSubscriptions =
+  Sub.batch
+    -- [ E.onResize Resized
+    [
+    --   E.onKeyUp (D.map (KeyChanged False) (D.field "key" D.string))
+    -- , E.onKeyDown (D.map (KeyChanged True) (D.field "key" D.string))
+    -- , 
+      E.onAnimationFrame Tick
+    -- , E.onVisibilityChange VisibilityChanged
+    -- , E.onClick (D.succeed MouseClick)
+    , E.onMouseDown (D.succeed (MouseButton True))
+    , E.onMouseUp (D.succeed (MouseButton False))
+    , E.onMouseMove (D.map2 MouseMove (D.field "pageX" D.float) (D.field "pageY" D.float))
+    ]
+
+-- Observation function
+
+observe {latent, objects} =
   let
-    image = Engine.render state gamewidth gameheight
+    image = Engine.render objects gamewidth gameheight
   in
   Render.view image htmlwidth htmlheight
 
 
+-- pomdpUpdate : (Computer -> memory -> memory) -> Msg -> Game memory -> Game memory
+pomdpUpdate updateMemory msg (Game vis memory computer) =
+  case msg of
+    Tick time ->
+      Game vis (updateMemory computer memory) <|
+        if computer.mouse.click
+        then { computer | time = Time time, mouse = mouseClick False computer.mouse }
+        else { computer | time = Time time }
+
+    GotViewport {viewport} ->
+      Game vis memory { computer | screen = toScreen viewport.width viewport.height }
+
+    Resized w h ->
+      Game vis memory { computer | screen = toScreen (toFloat w) (toFloat h) }
+
+    KeyChanged isDown key ->
+      Game vis memory { computer | keyboard = updateKeyboard isDown key computer.keyboard }
+
+    MouseMove pageX pageY ->
+      let
+        x = computer.screen.left + pageX
+        y = computer.screen.top - pageY
+      in
+      Game vis memory { computer | mouse = mouseMove x y computer.mouse }
+
+    MouseClick ->
+      Game vis memory { computer | mouse = mouseClick True computer.mouse }
+
+    MouseButton isDown ->
+      Game vis memory { computer | mouse = mouseDown isDown computer.mouse }
+
+    VisibilityChanged visibility ->
+      Game visibility memory
+        { computer
+            | keyboard = emptyKeyboard
+            , mouse = Mouse computer.mouse.x computer.mouse.y False False
+        }
+
+
+subscriptions model =
+  Sub.none  
+
 -- A POMDP 
-pomdp observeF updateF initF = 
+pomdp ({objects, latent} as scene) dynamics =
+  let
+    init () = 
+      (scene, Cmd.none)
+
+    update msg model =
+      ( pomdpUpdate scene msg model
+      , Cmd.none
+      )
+  in
   Browser.element
   { init = init
   , update = update
-  , subscriptions = subscriptions
-  , view = observeF
+  , subscriptions = pomdpSubscriptions
+  , view = observe
   }
