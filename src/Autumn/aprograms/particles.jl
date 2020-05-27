c17ab6cf505fe389b3032efbf99e189ae21cc9d9
Ria Das <riadas@mit.edu>
2020-05-27 15:22:28 -0400

    writing particles.jl WIP

--- src/Autumn/aprograms/particles.jl
+++ src/Autumn/aprograms/particles.jl
@@ -1,4 +1,67 @@
-# 
+using Distributions
+
+# --- types ---
+struct Position
+  x1::Int
+  x2::Int
+end
+
+struct Click
+  x1::Int
+  x2::Int
+end
+
+struct Particle
+  x1::Position
+  id::Int # based on global counter variable
+end
+
+# --- global variables ---
+const GRID_SIZE = 16
+particles = []
+
+# --- helper functions ---
+function nparticles
+  length(particles)
+end
+
+function isFree(position::Position)::Bool
+  length(filter(particle -> particle.x1 == position, particles)) == 0
+end
+
+function isWithinBounds(position::Position)::Bool
+  if (position.x1 >= 0 && position.x1 < GRID_SIZE && position.x2 >= 0 && position.x2 < GRID_SIZE)  
+    true
+  else
+    false
+  end
+end
+
+function adjacentPositions(position::Position)
+  x = position.x1
+  y = position.x2
+  positions = [Position(x + 1, y), Position(x - 1, y), Position(x, y + 1), Position(x, y - 1)]
+  filter(isWithinBounds, positions)
+end
+
+function nextParticle(particle::Particle)::Position
+  freePositions = filter(isFree, adjacentPositions(particle.x1))
+  if freePositions == []
+    particle
+  else
+    newPosition = freePositions[rand(Categorical(ones(length(freePositions))/length(freePositions)))]
+    Particle(newPosition, particle.id)
+  end
+end
+
+function particleGen(position::Position)::Particle
+
+end
+
+# history dictionary, keyed by index
+
+
+#=""
 struct Stream
 
 "History of values"
@@ -42,3 +105,4 @@ next fibonnaci =
 #  This looks a lot like the same problem as omega
 # - 
 # - Type stability will be hard
+=#
