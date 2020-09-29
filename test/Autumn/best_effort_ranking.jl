35c5e9426e8e44bb1b4970814932ab838e930d06
Elizabeth <elizabethweeks22@gmail.com>
2020-09-28 20:58:41 -0400

    updated test file

--- test/Autumn/best_effort_ranking.jl
+++ test/Autumn/best_effort_ranking.jl
@@ -29,6 +29,19 @@ aexpr2 = au"""(program
 (on clicked (= particles (addObj (prev particles) (Particle (Position (.. click x) (.. click y))))))
 )"""
 
+aexpr3 = au"""(program
+(= GRID_SIZE 16)
+
+(object Particle (Cell 0 0 "red"))
+
+(: particles (List Particle))
+(= particles
+(initnext (list)
+(updateObj (prev particles) (--> obj (Particle (uniformChoice (adjPositions (.. obj origin))))))))
+
+(on clicked (= particles (addObj (prev particles) (Particle (Position (.. click x) (.. click y))))))
+)"""
+
 aumod = eval(compiletojulia(aexpr))
 state = aumod.init(nothing, nothing, nothing, nothing, nothing, MersenneTwister(0))
 state = aumod.next(state, nothing, nothing, nothing, nothing, nothing)
@@ -46,6 +59,7 @@ e = [(aum -> [aum.Cell(aum.Position(5, 5), "blue", 0.8)],
 [nothing, nothing, nothing, nothing, nothing],
 [aumod2.Click(5,4), nothing, nothing, nothing, nothing]])]
 
+@inferred get_fitness(aumod2, e, aexpr2)
 fitness_p1 = get_fitness(aumod2, e, aexpr2)
 
 e = [(aum -> [aum.Cell(aum.Position(5, 5), "blue", 0.8)],
@@ -58,14 +72,15 @@ fitness_p2 = get_fitness(aumod2, e, aexpr2)
 
 @test fitness_p1 > fitness_p2
 
-# Test.@inferred
-#
-# update the notion document with what
-# I have done email the plan
-#
-# Waiting on ria to add new autumn (covid) model to the website
-# Kate and I are implementing the javascript based on the Kate model
-#
-# Come up with new tasks
-#
-# What is synthesizing the programs? To come
+aumod3 = eval(compiletojulia(aexpr3))
+
+e = [(aum -> [aum.Cell(aum.Position(5, 5), "blue", 0.8)],
+[[nothing, nothing, nothing, nothing, nothing, MersenneTwister(0)],
+[nothing, nothing, nothing, nothing, nothing],
+[aumod3.Click(5,5), nothing, nothing, nothing, nothing]])]
+
+@inferred get_fitness(aumod3, e, aexpr3)
+fitness_p3 = get_fitness(aumod3, e, aexpr3)
+
+@test fitness_p3 > fitness_p1
+@test fitness_p3 > fitness_p2
