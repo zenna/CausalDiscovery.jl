a9143ba04a21779e999e7353c3b8b51ef58de4f8
Ria Das <riadas@mit.edu>
2021-11-15 23:35:30 -0500

    change to run on linux

--- src/Autumn/generativemodel/full_eval.sh
+++ src/Autumn/generativemodel/full_eval.sh
@@ -2,27 +2,27 @@
 
 curr_date=$(date '+%Y-%m-%d_%H:%M:%S')
 algorithms=("heuristic" "sketch" "sketch_SINGLE")
-num_repeats=1
+num_repeats=2
 
-# model_names=("paint"
-#              "wind" 
-#              "sand"
-#              "bullets"
-#              "gravity_i"
-#              "gravity_iii"
-#              "disease"
-#              "gravity_ii"
-#              "count_1"
-#              "count_2"
-#              "double_count_1"
-#              "double_count_2"
-#              "mario"
-#              "count_3"
-#              "count_4"
-#             )
+model_names=("paint"
+             "wind" 
+             "sand"
+             "bullets"
+             "gravity_i"
+             "gravity_iii"
+             "disease"
+             "gravity_ii"
+             "count_1"
+             "count_2"
+             "double_count_1"
+             "double_count_2"
+             "mario"
+             "count_3"
+             "count_4"
+            )
 
-model_names=("paint")
-# "space_invaders"
+# model_names=("paint")
+# # "space_invaders"
 
 for model_name in ${model_names[@]}
 do
@@ -31,7 +31,7 @@ do
     for (( i = 1 ; i <= $num_repeats; i++ )) ### Inner for loop ###
     do
       echo "model_name: $model_name, algorithm: $algorithm, repeat: $i "
-      gtimeout 14400 julia --project=. src/Autumn/generativemodel/full_eval_$algorithm.jl $model_name $curr_date $i
+      timeout 18000 /scratch/riadas/julia-1.5.3/bin/julia --project=. src/Autumn/generativemodel/full_eval_$algorithm.jl $model_name $curr_date $i
     done  
   done
 done
