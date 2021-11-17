c0d0b7008d61f8e579c5f4ac5701fcc190453c5a
Ria Das <riadas@mit.edu>
2021-11-17 10:54:12 -0500

    fixed minor bug causing long runtimes

--- src/Autumn/generativemodel/scripts/full_eval.sh
+++ src/Autumn/generativemodel/scripts/full_eval.sh
@@ -4,24 +4,24 @@ curr_date=$(date '+%Y-%m-%d_%H:%M:%S')
 algorithms=("heuristic" "sketch_multi" "sketch_single")
 num_repeats=1
 
-model_names=("paint"
-             "sand"
-             "bullets"
-             "gravity_i"
-             "gravity_iii"
-             "disease"
-             "gravity_ii"
-             "count_1"
-             "count_2"
-             "double_count_1"
-             "double_count_2"
-             "mario"
-             "count_3"
-             "count_4"
-             "wind" 
-            )
+# model_names=("paint"
+#              "sand"
+#              "bullets"
+#              "gravity_i"
+#              "gravity_iii"
+#              "disease"
+#              "gravity_ii"
+#              "count_1"
+#              "count_2"
+#              "double_count_1"
+#              "double_count_2"
+#              "mario"
+#              "count_3"
+#              "count_4"
+#              "wind" 
+#             )
 
-# model_names=("paint")
+model_names=("sand")
 # # "space_invaders"
 
 for model_name in ${model_names[@]}
@@ -35,6 +35,9 @@ do
     done  
   done
 done
+
+# julia --project=. src/Autumn/generativemodel/scripts/compute_output_accuracy.jl
+# julia --project=. src/Autumn/generativemodel/scripts/compute_average_accuracies.jl
 
 /scratch/riadas/julia-1.5.3/bin/julia --project=. src/Autumn/generativemodel/scripts/compute_output_accuracy.jl
 /scratch/riadas/julia-1.5.3/bin/julia --project=. src/Autumn/generativemodel/scripts/compute_average_accuracies.jl
