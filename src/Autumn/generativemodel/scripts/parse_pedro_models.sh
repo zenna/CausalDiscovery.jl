0474310ded2d2129e2cba45e27bfc745c8f7a326
Ria Das <riadas@mit.edu>
2022-02-17 02:26:05 -0500

    added forgotten scripts

---
+++ src/Autumn/generativemodel/scripts/parse_pedro_models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+game_names=(""
+            ""
+            ""
+            ""
+            "")
+
+
+for game_name in ${game_names[@]}
+do
+  echo "model_name: $game_name "
+  nohup /scratch/riadas/julia-1.5.3/bin/julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name > PEDRO_PARSING_LOGS/$game_name.out &
+done
+
+#  julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name
