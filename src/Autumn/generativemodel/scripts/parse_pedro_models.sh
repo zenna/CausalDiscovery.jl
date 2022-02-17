617aef1e4316d0478e6cf14a1cc9f34c0b41cf7e
Ria Das <riadas@mit.edu>
2022-02-17 02:43:42 -0500

    fixed julia version

--- src/Autumn/generativemodel/scripts/parse_pedro_models.sh
+++ src/Autumn/generativemodel/scripts/parse_pedro_models.sh
@@ -10,7 +10,7 @@ game_names=(""
 for game_name in ${game_names[@]}
 do
   echo "model_name: $game_name "
-  nohup /scratch/riadas/julia-1.5.3/bin/julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name > PEDRO_PARSING_LOGS/$game_name.out &
+  nohup /scratch/riadas/julia-1.5.4/bin/julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name > PEDRO_PARSING_LOGS/$game_name.out &
 done
 
 #  julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name
