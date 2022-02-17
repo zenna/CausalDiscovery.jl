96ca77d8e578f666a01abe9a07ba18e393cc26e5
Ria Das <riadas@mit.edu>
2022-02-17 11:37:04 -0500

    added print statements

--- src/Autumn/generativemodel/scripts/parse_pedro_model.jl
+++ src/Autumn/generativemodel/scripts/parse_pedro_model.jl
@@ -20,8 +20,9 @@ small_matrix, small_unformatted_matrix, small_object_decomposition, small_prev_u
 small_x = (small_matrix, small_unformatted_matrix, small_object_decomposition, small_prev_used_rules)
 small_existing_files = readdir(small_directory_name)
 JLD.save("$(small_directory_name)/data_$(length(small_existing_files)).jld", "data", small_x)
-
+println("DONE SMALL!")
 matrix, unformatted_matrix, object_decomposition, prev_used_rules = singletimestepsolution_matrix(observations, user_events, grid_size, singlecell=false, pedro=true, upd_func_space=6)
 x = (matrix, unformatted_matrix, object_decomposition, prev_used_rules)
 existing_files = readdir(directory_name)
 JLD.save("$(directory_name)/data_$(length(existing_files)).jld", "data", x)
+println("DONE LARGE!")
