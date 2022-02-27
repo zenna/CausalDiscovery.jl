1f483b354734f7ab20782a2dd91f99806a62ee4a
Ria A Das <riadas@sketch4.csail.mit.edu>
2022-02-26 20:10:11 -0500

    pre-merge

--- src/Autumn/generativemodel/scripts/parse_pedro_models.sh
+++ src/Autumn/generativemodel/scripts/parse_pedro_models.sh
@@ -18,6 +18,5 @@ for game_name in ${game_names[@]}
 do
   echo "model_name: $game_name "
   nohup /scratch/riadas/julia-1.5.4/bin/julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name > PEDRO_PARSING_LOGS/$game_name.out &
-done
-
+done	
 #  julia --project=. src/Autumn/generativemodel/scripts/parse_pedro_model.jl $game_name
