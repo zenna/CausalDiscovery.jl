9d7e8ddc1f2b4411eebaa6257092344ccb725ba8
Zenna Tavares <zennatavares@gmail.com>
2023-03-16 13:23:50 -0400

    just include generative model

--- src/CausalDiscovery.jl
+++ src/CausalDiscovery.jl
@@ -1,13 +1,6 @@
 module CausalDiscovery
 using Reexport
 
-include("Autumn/Autumn.jl")
-@reexport using .Autumn
-
-# include("CISC/CISC.jl")
-# include("CISC.jl")
-# include("MCMC.jl/model.jl")
-# include("MCMC.jl/grammar.jl")
-
+include("Autumn/generativemodel/GenerativeModel2.jl")
 
 end # module
