5572ed75118311209c29f7e26b4cf2de9fe6f21c
Ria Das <riadas@mit.edu>
2023-04-06 19:09:24 -0400

    fixed cisc script

--- evaluation/cisc/scripts/full_eval.sh
+++ evaluation/cisc/scripts/full_eval.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 curr_date=$(date '+%Y-%m-%d_%H:%M:%S')
-algorithms=("heuristic"
-            "sketch_multi"
-            "sketch_single")
-# algorithms=("sketch_multi")
+# algorithms=("heuristic"
+#             "sketch_multi"
+#             "sketch_single")
+algorithms=("heuristic")
 num_repeats=3
 
 # model_names=("paint"
@@ -56,51 +56,51 @@ model_names=("paint"
              "gravity_ii"
              "count_1"
              "count_2"
-	     "count_3"
-	     "count_4"
-	     "count_5"
-             "double_count_1"
-	     "double_count_2"
-             "mario"
-             "wind" 
-	     "ice"
-	     "particles"
-	     "ants"
-	     "chase"
-	     "magnets_i"
-	     "space_invaders"
-	     "sokoban_i"
-	     "water_plug"
+            "count_3"
+            "count_4"
+            "count_5"
+            "double_count_1"
+            "double_count_2"
+            "mario"
+            "wind" 
+            "ice"
+            "particles"
+            "ants"
+            "chase"
+            "magnets_i"
+            "space_invaders"
+            "sokoban_i"
+            "water_plug"
             )
 
-model_names=("paint"
-             "lights"
-             "sand"
-             "disease"
-             "grow"
-             "bullets"
-             "gravity_i"
-             "gravity_iii"
-             "gravity_iv"
-             "gravity_ii"
-             "count_1"
-             "count_2"
-             "double_count_1"
-             "mario"
-             "wind"
-             "water_plug"
-            )
+# model_names=("paint"
+#              "lights"
+#              "sand"
+#              "disease"
+#              "grow"
+#              "bullets"
+#              "gravity_i"
+#              "gravity_iii"
+#              "gravity_iv"
+#              "gravity_ii"
+#              "count_1"
+#              "count_2"
+#              "double_count_1"
+#              "mario"
+#              "wind"
+#              "water_plug"
+#             )
 
-# "water_plug"
-# model_names=("ice"
-#              "particles"
-#              "ants"
-#              "chase"
-#              "magnets"
-#              "space_invaders"
-#              "sokoban")
-# # "space_invaders"
-model_names=("sand")
+# # "water_plug"
+# # model_names=("ice"
+# #              "particles"
+# #              "ants"
+# #              "chase"
+# #              "magnets"
+# #              "space_invaders"
+# #              "sokoban")
+# # # "space_invaders"
+# model_names=("ice")
 
 for  (( i = 1 ; i <= $num_repeats; i++ ))
 do
@@ -109,7 +109,7 @@ do
     for model_name in ${model_names[@]} ### Inner for loop ###
     do
       echo "model_name: $model_name, algorithm: $algorithm, repeat: $i "
-      nohup timeout 86400 /scratch/riadas/julia-1.5.4/bin/julia --project=. evaluation/cisc/scripts/full_eval.jl $model_name $algorithm $curr_date $i > OCTOBER_TESTING_OUTS/$model_name.$algorithm.out & 
+      nohup timeout 86400 julia --project=. evaluation/cisc/scripts/full_eval.jl $model_name $algorithm $curr_date $i > /dev/null & # OCTOBER_TESTING_OUTS/$model_name.$algorithm.out & 
       sleep 60
     done
     sleep 28800  
