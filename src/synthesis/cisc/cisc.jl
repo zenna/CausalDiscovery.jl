6b2e45f104181512215d6d7465053746cc26d0ca
Ria Das <riadas@mit.edu>
2023-06-23 00:32:25 -0400

    more cleanup

--- src/synthesis/cisc/cisc.jl
+++ src/synthesis/cisc/cisc.jl
@@ -2,7 +2,14 @@ import Pkg; Pkg.add("Pickle")
 using Autumn
 using JLD 
 using Dates
-include("functional_synthesis/full_synthesis.jl")
+include("functional_synthesis/singletimestepsolution.jl");
+include("automata_synthesis/heuristic_automata_synthesis.jl")
+include("automata_synthesis/sketch_automata_synthesis.jl")
+include("automata_synthesis/sketch_multi_automata_synthesis.jl")
+
+function generate_observations(model_name::String)
+  JLD.load("test/cisc/data/observed/single_trace_observations/$(model_name).jld", "data")
+end
 
 function run_model(model_name::String, algorithm, iteration=1, desired_per_matrix_solution_count=1, desired_solution_count=1; multi_trace=false, indices=[], observations_tup=[])
   run_id = string(model_name, "_", algorithm, "_", iteration)
@@ -283,4 +290,132 @@ function run_model_multi_trace(model_name, singlecell=false, pedro=false; indice
     solutions = []
   end
   solutions
+end
+
+function synthesize_program_given_decomp(run_id, random, decomp, observation_tuple, global_event_vector_dict, redundant_events_set; 
+                                          pedro = false,
+                                          desired_solution_count = 1, # 2
+                                          desired_per_matrix_solution_count = 1, # 5
+                                          interval_painting_param = false, 
+                                          upd_func_spaces = [1],
+                                          z3_option = "none",
+                                          time_based=false,
+                                          co_occurring_param=false, 
+                                          transition_param=false,
+                                          algorithm="heuristic",
+                                          sketch_timeout=0,
+                                          stop_times=[]) 
+
+  @show run_id
+  program_strings = []
+
+  # reset global_event_vector_dict and redundant_events_set for each new context:
+  # remove events dealing with global or object-specific state
+  for event in keys(global_event_vector_dict)
+    if occursin("globalVar", event) || occursin("field1", event)
+      delete!(global_event_vector_dict, event)
+    end
+  end
+
+  for event in redundant_events_set 
+    if occursin("globalVar", event) || occursin("field1", event)
+      delete!(redundant_events_set, event)
+    end
+  end
+  observations, user_events, grid_size = observation_tuple                               
+  matrix, unformatted_matrix, object_decomposition, prev_used_rules = decomp                                        
+
+  if algorithm == "heuristic"
+    solutions = generate_on_clauses_GLOBAL(run_id, random, matrix, unformatted_matrix, object_decomposition, user_events, global_event_vector_dict, redundant_events_set, grid_size, desired_solution_count, desired_per_matrix_solution_count, interval_painting_param, false, z3_option, time_based, 0, sketch_timeout, co_occurring_param, transition_param, stop_times=stop_times)
+  elseif algorithm == "sketch_single"
+    solutions = generate_on_clauses_SKETCH_SINGLE(run_id, random, matrix, unformatted_matrix, object_decomposition, user_events, global_event_vector_dict, redundant_events_set, grid_size, desired_solution_count, desired_per_matrix_solution_count, interval_painting_param, z3_option, time_based, 0, sketch_timeout, co_occurring_param, transition_param, stop_times=stop_times)
+  elseif algorithm == "sketch_multi"
+    solutions = generate_on_clauses_SKETCH_MULTI(run_id, random, matrix, unformatted_matrix, object_decomposition, user_events, global_event_vector_dict, redundant_events_set, grid_size, desired_solution_count, desired_per_matrix_solution_count, interval_painting_param, z3_option, time_based, 0, sketch_timeout, co_occurring_param, transition_param, stop_times=stop_times)
+  else 
+    error("algorithm $(algorithm) does not exist")
+  end
+
+  for solution in solutions 
+    if solution[1] != [] 
+      on_clauses, new_object_decomposition, global_var_dict = solution
+      # # @show on_clauses 
+      
+      program = full_program_given_on_clauses(on_clauses, new_object_decomposition, global_var_dict, grid_size, matrix)
+      push!(program_strings, program)
+    end
+  end
+  program_strings                              
+end
+
+function synthesize_program(model_name::String; 
+                            singlecell = false,
+                            pedro = false,
+                            desired_solution_count = 1, # 2
+                            desired_per_matrix_solution_count = 1, # 5
+                            interval_painting_param = false, 
+                            upd_func_spaces = [1],
+                            z3_option = "none",
+                            time_based=false,
+                            co_occurring_param=false, 
+                            transition_param=false,
+                            algorithm="heuristic",
+                            sketch_timeout=0,
+                            )
+  
+  run_id = string(model_name, "_", algorithm)
+
+  if pedro 
+    observations, user_events, grid_size = generate_observations_pedro(model_name)
+  else
+    observations, user_events, grid_size = generate_observations(model_name)
+  end
+
+  program_strings = []
+  global_event_vector_dict = Dict()
+  redundant_events_set = Set()
+  for upd_func_space in upd_func_spaces # 1, 2, 3 
+    matrix, unformatted_matrix, object_decomposition, prev_used_rules = singletimestepsolution_matrix(observations, user_events, grid_size, singlecell=singlecell, pedro=pedro, upd_func_space=upd_func_space)    
+    if algorithm == "heuristic"
+      solutions = generate_on_clauses_GLOBAL(run_id, matrix, unformatted_matrix, object_decomposition, user_events, global_event_vector_dict, redundant_events_set, grid_size, desired_solution_count, desired_per_matrix_solution_count, interval_painting_param, false, z3_option, time_based, 0, sketch_timeout, co_occurring_param, transition_param)
+    elseif algorithm == "sketch_single"
+      solutions = generate_on_clauses_SKETCH_SINGLE(run_id, matrix, unformatted_matrix, object_decomposition, user_events, global_event_vector_dict, redundant_events_set, grid_size, desired_solution_count, desired_per_matrix_solution_count, interval_painting_param, z3_option, time_based, 0, sketch_timeout, co_occurring_param, transition_param)
+    elseif algorithm == "sketch_multi"
+      solutions = generate_on_clauses_SKETCH_MULTI(run_id, matrix, unformatted_matrix, object_decomposition, user_events, global_event_vector_dict, redundant_events_set, grid_size, desired_solution_count, desired_per_matrix_solution_count, interval_painting_param, z3_option, time_based, 0, sketch_timeout, co_occurring_param, transition_param)
+    else 
+      error("algorithm $(algorithm) does not exist")
+    end
+    # # @show solutions
+    for solution in solutions 
+      if solution[1] != [] 
+        on_clauses, new_object_decomposition, global_var_dict = solution
+        # # @show on_clauses 
+        
+        program = full_program_given_on_clauses(on_clauses, new_object_decomposition, global_var_dict, grid_size, matrix)
+        push!(program_strings, program)
+      end
+    end
+    
+    # if length(program_strings) >= desired_solution_count
+    #   break
+    # end    
+  end
+  
+  # open("/Users/riadas/Documents/urop/CausalDiscovery.jl/src/synthesis/cisc/workshop/$(model_name).txt","w") do io
+  #   if model_name != "ants" 
+  #     println(io, program_strings[1])
+  #   else
+  #     println(io, program_strings[2])
+  #   end
+  # end
+
+  # open("/Users/riadas/Documents/urop/CausalDiscovery.jl/src/synthesis/cisc/conference/$(model_name).txt","a") do io
+  #   println(io, string("BEGIN UPDATE FUNC SPACE: ", upd_func_spaces[1], "\n\n\n"))
+  #   for program_string in program_strings
+  #     println(io, program_string)
+  #     println(io, "\n\n\n")
+  #   end
+  #   println(io, "END UPDATE FUNC SPACE: $(upd_func_spaces[1]) \n\n\n")
+  # end
+
+  program_strings
 end
--- src/synthesis/cisc/functional_synthesis/singletimestepsolution.jl
+++ src/synthesis/cisc/functional_synthesis/singletimestepsolution.jl
@@ -5,8 +5,6 @@ using Random
 using Pickle
 include("generativemodel.jl")
 include("../automata_synthesis/state_construction_utils.jl")
-include("construct_observation_data.jl")
-include("multi_trace_perception.jl")
 
 displacement_dict = Dict((0, 0) => "(= objX objX)", 
                          (0, 1) => "(= objX (moveDown objX))", 
@@ -59,68 +57,17 @@ function singletimestepsolution_matrix(observations, old_user_events, grid_size;
   end
   
   # SEED PREV USED RULES FOR EFFIENCY AT THE MOMENT 
-  prev_used_rules = [ "(= objX objX)",
+  prev_used_rules = ["(= objX objX)",
                       "(= objX (nextLiquid objX))",
                       "(= objX (moveDown objX))",
                       "(= objX (moveUp objX))",
                       "(= objX (moveLeft objX))",
                       "(= objX (moveRight objX))",
-                    ] 
-
-  if upd_func_space == 2 
-    prev_used_rules = ["(= objX objX)",
-                       "(= objX (moveUpNoCollision objX))",
-                       "(= objX (moveDownNoCollision objX))",
-                       "(= objX (moveLeftNoCollision objX))",
-                       "(= objX (moveRightNoCollision objX))",]
-  end
-
-  if upd_func_space == 3 
-    prev_used_rules = ["(= objX objX)",
-                       "(= objX (nextLiquid objX))",
-                       "(= objX (moveUpNoCollision objX))",
-                       "(= objX (moveDownNoCollision objX))",
-                       "(= objX (moveLeftNoCollision objX))",
-                       "(= objX (moveRightNoCollision objX))",
-                       ]
-  end
-
-  if upd_func_space in [4, 5] 
-    prev_used_rules = ["(= objX objX)",
-                        "(= objX (moveDown objX))",
-                        "(= objX (moveUp objX))",
-                        "(= objX (moveLeft objX))",
-                        "(= objX (moveRight objX))",
-                        "(= objX (moveLeftNoCollision (moveUpNoCollision objX)))",
-                        "(= objX (moveLeftNoCollision (moveDownNoCollision objX)))",
-                        "(= objX (moveRightNoCollision objX))",
-                        "(= objX (moveRightNoCollision (moveUpNoCollision objX)))",
-                        "(= objX (moveRightNoCollision (moveDownNoCollision objX)))",
-                        "(= objX (moveUpNoCollision objX))",
-                        "(= objX (moveDownNoCollision objX))",
-                        "(= objX (moveLeftNoCollision objX))",
-                        "(= objX (moveRightNoCollision objX))",
-                      ]
-  end
-
-  if upd_func_space in [6]  
-    prev_used_rules = ["(= objX objX)",
-                        "(= objX (nextLiquid objX))",
-                        "(= objX (moveDown objX))",
-                        "(= objX (moveUp objX))",
-                        "(= objX (moveLeft objX))",
-                        "(= objX (moveRight objX))",
-                        # "(= objX (moveLeftNoCollision (moveUpNoCollision objX)))",
-                        # "(= objX (moveLeftNoCollision (moveDownNoCollision objX)))",
-                        # "(= objX (moveRightNoCollision objX))",
-                        # "(= objX (moveRightNoCollision (moveUpNoCollision objX)))",
-                        # "(= objX (moveRightNoCollision (moveDownNoCollision objX)))",
-                        "(= objX (moveUpNoCollision objX))",
-                        "(= objX (moveDownNoCollision objX))",
-                        "(= objX (moveLeftNoCollision objX))",
-                        "(= objX (moveRightNoCollision objX))",
-                      ]
-  end
+                      "(= objX (moveUpNoCollision objX))",
+                      "(= objX (moveDownNoCollision objX))",
+                      "(= objX (moveLeftNoCollision objX))",
+                      "(= objX (moveRightNoCollision objX))",
+                    ]
 
   max_iters = length(prev_used_rules)
 
@@ -710,6 +657,114 @@ function parse_and_map_objects(observations, gridsize=16; singlecell=false, pedr
   # end
 
   (object_types, object_mapping, "white", gridsize)  
+end
+
+function parse_and_map_objects_multiple_traces(multiple_traces, grid_size=16; singlecell=false, pedro=false)
+  object_decomposition_list = []
+
+  for observations in multiple_traces
+    object_decomposition = parse_and_map_objects(observations, grid_size, singlecell=singlecell, pedro=pedro)
+    push!(object_decomposition_list, object_decomposition)
+  end
+
+  # take union of object types across object_decompositions, and reassign object_mapping type_id's accordingly
+  object_types, object_mapping, background, grid_size = object_decomposition_list[1]
+  for object_decomposition in object_decomposition_list[2:end]
+    curr_object_types, curr_object_mapping, _, _ = object_decomposition 
+    for type in curr_object_types 
+      
+      if !((type.shape, type.color) in map(t -> (t.shape, t.color), object_types))
+        old_type_id = type.id
+        type.id = length(object_types) + 1
+        push!(object_types, type)
+      end
+
+    end
+  end
+
+  # re-assign type id's of objects with type
+  for object_decomposition in object_decomposition_list[2:end]
+    _, curr_object_mapping, _, _ = object_decomposition
+    for id in collect(keys(curr_object_mapping))
+      for time in 1:length(curr_object_mapping[id])
+        obj = curr_object_mapping[id][time]
+        if !isnothing(obj)
+          type = filter(t -> (t.shape, t.color) == (obj.type.shape, obj.type.color), object_types)[1]
+          curr_object_mapping[id][time].type = type
+        end
+      end
+    end
+  end
+
+  # construct full (long) object_mapping by linking existing objects across object_mappings, and creating new id's
+  # for added object_id's in traces 2 through (n-1)
+  prior_observations_count = length(multiple_traces[1])
+
+  linked_ids = Dict()
+
+  for i in 2:length(object_decomposition_list)
+    prev_object_decomposition = object_decomposition_list[i - 1]
+    # _, prev_object_mapping, _, _ = prev_object_decomposition
+
+    object_decomposition = object_decomposition_list[i]
+    _, curr_object_mapping, _, _ = object_decomposition 
+    curr_ids = sort(collect(keys(curr_object_mapping)))
+    for id in curr_ids 
+      if !isnothing(curr_object_mapping[id][1]) # object exists at start
+        if !(nothing in object_mapping[id]) # CASE 1: object always exists in this prior and curr object_mapping 
+          push!(object_mapping[id], curr_object_mapping[id]...)
+        else # CASE 2: object is removed in prior object_mapping
+          type = curr_object_mapping[id][1].type
+          object_ids_with_type = filter(id -> filter(obj -> !isnothing(obj), curr_object_mapping[id])[1].type.id == type.id, collect(keys(curr_object_mapping)))
+          # CASE 2a: object is a singleton object (not contained in list): can keep the same id 
+          if length(object_ids_with_type) == 1 
+            push!(object_mapping[id], curr_object_mapping[id]...)
+          else # CASE 2b: object is in a list: create a new id for the object in curr object_mapping
+            new_id = length(collect(keys(object_mapping))) + 1
+
+            for time in 1:length(curr_object_mapping[id])
+              if !isnothing(curr_object_mapping[id][time])
+                curr_object_mapping[id][time].id = new_id
+              end 
+            end
+            
+            object_mapping[new_id] = vcat([nothing for j in 1:prior_observations_count]..., curr_object_mapping[id]...)
+            linked_ids[(id, i - 1)] = (new_id, i)
+          end
+
+        end
+      else # CASE 3: object does not exist at start of prev object_mapping (or curr object_mapping): create new id for added object
+        new_id = length(collect(keys(object_mapping))) + 1 
+
+        for time in 1:length(curr_object_mapping[id])
+          if !isnothing(curr_object_mapping[id][time])
+            curr_object_mapping[id][time].id = new_id
+          end 
+        end
+
+        object_mapping[new_id] = vcat([nothing for j in 1:prior_observations_count]..., curr_object_mapping[id]...)
+      end
+    end
+    prior_observations_count += length(multiple_traces[i])
+    
+    for id in keys(object_mapping)
+      if length(object_mapping[id]) != prior_observations_count
+        missing_count = prior_observations_count - length(object_mapping[id])
+        object_mapping[id] = vcat(object_mapping[id]..., [nothing for j in 1:missing_count]...)
+      end
+    end
+  end
+
+  # num_observations = sum(map(x -> length(x), observations))
+  # for id in keys(object_mapping)
+  #   if length(object_mapping[id]) != num_observations
+  #     for i in (length(object_mapping[id]) + 1):num_observations
+  #       push!(object_mapping[id], nothing)
+  #     end
+  #   end
+  # end
+
+  object_types, object_mapping, background, grid_size, linked_ids
 end
 
 function compute_closest_objects(curr_objects, next_objects, object_mapping, time, grid_size, unitSize)
